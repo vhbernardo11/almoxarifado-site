230ea88a45ce7b11581c7a6b9689aecb22d0cdb0
vhbernardo11 <vhbernardo11@gmail.com>
2026-09-04 16:53:37 -0300

    Carrega Bolsa Família R3 com itens padrão por família

--- esf06-producao/bolsa-familia-r1.js
+++ esf06-producao/bolsa-familia-r1.js
@@ -1,1 +1,1 @@
-(()=>{'use strict';const s=document.createElement('script');s.src='./bolsa-familia-r2.js?rev=20260904-1545-r2b';s.async=false;document.head.appendChild(s)})();
+(()=>{'use strict';const s=document.createElement('script');s.src='./bolsa-familia-r3.js?rev=20260904-1652-r3';s.async=false;document.head.appendChild(s)})();
